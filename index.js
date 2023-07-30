b2c11295aefc97f8024604458694919b2e711f9d
Mostafizur-pro <mostafizur0195@gmail.com>
2023-07-30 22:27:23 +0600

    some edit

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`;
 const uri =
   "mongodb+srv://master-tech:K4QhoBCbnA75AHu0@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
@@ -27,6 +23,9 @@ async function run() {
     const productCollection = client
       .db("master-tech")
       .collection("AllProducts");
+    const categoryCollection = client
+      .db("master-tech")
+      .collection("categories");
 
     // ------------PRODUCT------------------------------
 
@@ -40,15 +39,28 @@ async function run() {
       const result = await productCollection.find({}).toArray();
       res.send(result);
     });
+    app.get("/categories", async (req, res) => {
+      const name = req.query.categoryName;
+      const query = { categoryName: name };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/category", async (req, res) => {
+      const query = {};
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
-run().catch(console.dir);
+
+run().catch(console.log);
 
 app.get("/", (req, res) => {
-  res.send(" Computer builder Server is running");
+  res.send("Computer builder server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Computer builder server is running on port ${port}`);
+  console.log(`Computer builder server running on ${port}`);
 });
